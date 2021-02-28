dd39c10834478ca8d9757fa39d9466bd569a06de
Alexey Gerasev <alexey.gerasev@gmail.com>
2021-02-28 10:56:53 +0700

    Navigation with mouse

--- src/display/composer.ts
+++ src/display/composer.ts
@@ -8,12 +8,93 @@ export class Composer {
   hlinks: Map<string, HorizontalLink>;
   vlinks: Map<string, VerticalLink>;
   html: JQuery<HTMLElement>;
+  anchor: JQuery<HTMLElement>;
 
-  constructor() {
+  width: number;
+  height: number;
+  position: [number, number];
+  zoom: number;
+  drag: boolean;
+
+  constructor(parent: JQuery<HTMLElement>) {
     this.nodes = new Map<string, PersonNode>();
     this.hlinks = new Map<string, HorizontalLink>();
     this.vlinks = new Map<string, VerticalLink>();
-    this.html = $("<div class='composer-box'></div>");
+    this.html = $(`
+    <div class='composer-container'>
+      <div class='composer-anchor'>
+      </div>
+    </div>
+    `);
+    this.anchor = this.html.find(".composer-anchor");
+    parent.append(this.html);
+
+    [this.width, this.height] = [0, 0];
+    this.position = [ 0.0, 0.0 ];
+    this.zoom = 1.0;
+    this.updateViewport();
+
+    this.html[0].onwheel = (e: WheelEvent) => {
+      e.preventDefault();
+      this.onZoom(e.deltaY, [e.clientX, e.clientY]);
+    };
+
+    this.drag = false;
+    this.html[0].onmousedown = (e: MouseEvent) => {
+      e.preventDefault();
+      this.drag = true;
+    };
+    let leaveCallback = (e: MouseEvent) => {
+      e.preventDefault();
+      this.drag = false;
+    };
+    this.html[0].onmouseup = leaveCallback;
+    this.html[0].onmouseleave = leaveCallback;
+    this.html[0].onmousemove = (e: MouseEvent) => {
+      if (this.drag) {
+        e.preventDefault();
+        this.onShift([e.movementX, e.movementY]);
+      }
+    };
+  }
+
+  screenToViewport([x, y]: [number, number]): [number, number] {
+    return [
+      this.zoom * (x - 0.5 * this.width) + this.position[0],
+      this.zoom * (y - 0.5 * this.height) + this.position[1],
+    ];
+  }
+  viewportToScreen([x, y]: [number, number]): [number, number] {
+    return [
+      (x - this.position[0]) / this.zoom + 0.5 * this.width,
+      (y - this.position[1]) / this.zoom + 0.5 * this.height,
+    ];
+  }
+  updatePosition() {
+    let [cx, cy] = this.viewportToScreen([0.0, 0.0]);
+    this.anchor.css("left", cx + "px");
+    this.anchor.css("top", cy + "px");
+    this.anchor.css("scale", (1.0 / this.zoom).toString());
+  }
+  updateViewport() {
+    let rect = this.html[0].getBoundingClientRect();
+    this.width = rect.width;
+    this.height = rect.height;
+    this.updatePosition();
+  }
+
+  onZoom(delta: number, [sx, sy] : [number, number]) {
+    let zoom = this.zoom * Math.pow(2.0, 0.1 * delta);
+    let dz = this.zoom - zoom;
+    this.position[0] += (sx - 0.5 * this.width) * dz;
+    this.position[1] += (sy - 0.5 * this.height) * dz;
+    this.zoom = zoom;
+    this.updatePosition();
+  }
+  onShift([dx, dy]: [number, number]) {
+    this.position[0] -= dx * this.zoom;
+    this.position[1] -= dy * this.zoom;
+    this.updatePosition();
   }
 
   createNode(person: Person, position: number, level: number): PersonNode {
@@ -22,7 +103,7 @@ export class Composer {
     } else {
       let node = new PersonNode(this, person, position, level);
       this.nodes.set(person.id, node);
-      this.html.append(node.html);
+      this.anchor.append(node.html);
       node.updatePosition();
       return node;
     }
@@ -34,7 +115,7 @@ export class Composer {
     } else {
       let hlink = new HorizontalLink(this, left, right);
       this.hlinks.set(id, hlink);
-      this.html.append(hlink.html);
+      this.anchor.append(hlink.html);
       hlink.updatePosition();
       return hlink;
     }
@@ -46,7 +127,7 @@ export class Composer {
     } else {
       let vlink = new VerticalLink(this, top, bottom);
       this.vlinks.set(id, vlink);
-      this.html.append(vlink.html);
+      this.anchor.append(vlink.html);
       vlink.updatePosition();
       return vlink;
     }
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ $(function () {
   $.get('data/tree.toml', function (tree_toml) {
     let tree = new Tree(toml.parse(tree_toml));
     console.log(tree);
-    let composer = new Composer();
-    $(document.body).append(composer.html);
-    composer.createNode(tree.persons.get("f88c5f4aec7a2a744ee003ac192f82a5")!, 1.0, 1);//.expandBottom();
+    let composer = new Composer($(document.body));
+    composer.createNode(tree.persons.get("17e71bf8371fc0696602148257e366ad")!, 0.0, 0);//.expandBottom();
   });
 });
